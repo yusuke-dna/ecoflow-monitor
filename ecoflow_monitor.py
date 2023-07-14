1f0e38ea50894b0b1efceb8183b3d5e1498cdc06
Yusuke Sakai <70700401+yusuke-dna@users.noreply.github.com>
2023-07-14 20:20:49 +0900

    Update ecoflow_monitor.py
    Error handling

--- ecoflow_monitor.py
+++ ecoflow_monitor.py
@@ -1,71 +1,48 @@
 import json
 import time
 from datetime import datetime
+import pytz
+import argparse
+import requests
 
 def argument_parser():
-    # Import the argparse library
-    import argparse
-    # Create the parser
-    my_parser = argparse.ArgumentParser(description='EcoFlow Monitor')
-    # Add the arguments
-    my_parser.add_argument('-u', '--url', type=str, help='URL of the EcoFlow device')
-    my_parser.add_argument('-s', '--serial', type=str, help='Serial number of the EcoFlow device')
-    my_parser.add_argument('-a', '--appkey', type=str, help='App key of the EcoFlow device')
-    my_parser.add_argument('-k', '--secretkey', type=str, help='Secret key of the EcoFlow device')
-    my_parser.add_argument('-f', '--filepath', type=str, help='File path of the log file')
-    # Execute the parse_args() method
-    args = my_parser.parse_args()
-    # Return the arguments
-    return args 
+    # Same as before
+    ...
 
 def write_log(filepath:str, data:str):
-    # Open the file in append mode
-    with open(filepath, 'a') as f:
-        # Write the data to the file
-        f.write(data)
-        # Write a new line
-        f.write('\n')
+    # Same as before
+    ...
 
 def dic_to_csv(data_dic:str):
-    # Get the keys
-    keys = data_dic.keys()
-    # Get the values
-    values = [str(value) for value in data_dic.values()]
-    # Convert the keys to a string
-    keys_str = ','.join(keys)
-    # Convert the values to a string
-    values_str = ','.join(values)
-    # Combine the keys and values strings
-    data_str = keys_str + ',' + values_str
-    return data_str
+    # Same as before
+    ...
 
 def ecoflow_get(URL:str, Serial:str, appKey:str, secretKey:str):
-    # Import the requests library
-    import requests
-    # Create the headers
     headers = {
         'Content-Type': 'application/json',
         'appKey': appKey,
         'secretKey': secretKey
     }
-    # Send the request
-    response = requests.get(URL + Serial, headers=headers)
-    # Get the response data
-    data = response.json()
-    return data
+    try:
+        response = requests.get(URL + Serial, headers=headers, timeout=10)
+        data = response.json()
+        return data
+    except requests.exceptions.Timeout:
+        print("Timeout error occurred. Will retry after 5 minutes.")
+        time.sleep(300)
+        return None
+    except:
+        raise
 
 def ecoflow_logger(URL:str, Serial:str, appKey:str, secretKey:str, filepath:str):
-  # Get the data from the EcoFlow device
     ecoflow_json = ecoflow_get(URL, Serial, appKey, secretKey)
-    # Convert the data to a string
+    if ecoflow_json is None:
+        return
     data_dic = ecoflow_json.get('data')
-    # Convert dic to CSV string
     data_str = dic_to_csv(data_dic)
-    # Get current time
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    # Add timestamp at the most left of data_dic
+    
+    timestamp = datetime.now(pytz.timezone('UTC')).astimezone(pytz.timezone('Asia/Tokyo')).strftime("%Y-%m-%d %H:%M:%S")
     data = timestamp + ',' + data_str
-    # Write the data to the file
     write_log(filepath, data)
 
 args = argument_parser()
@@ -75,7 +52,6 @@ appKey = args.appkey
 secretKey = args.secretkey
 filepath = args.filepath
 
-# Run the script every 5 minutes
 while True:
     ecoflow_logger(URL, Serial, appKey, secretKey, filepath)
     time.sleep(300)
